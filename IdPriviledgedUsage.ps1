9e1e24e6c2df896092e0e37493b632a524ad7c63
Bob Stobie <99285369+bstobie@users.noreply.github.com>
2022-02-23 14:08:38 -0600

    Update IdPriviledgedUsage.ps1

--- IdPriviledgedUsage.ps1
+++ IdPriviledgedUsage.ps1
@@ -1,18 +1,18 @@
 [CmdletBinding()]
 param (
-    [Parameter(Position=0,Mandatory=$false)][string]$ThycoticUser = "blusys",
-    [Parameter(Position=1,Mandatory=$false)][decimal]$TimeDelay = 1,
-    [Parameter(Position=2,Mandatory=$false)][string]$CompAcct = "localhost"
+    [parameter(Position = 0, Mandatory = $false)][string]$LMDomain = "corpid.net",
+    [Parameter(Position = 1, Mandatory = $false)][string]$CompAcct = "localhost",
+    [Parameter(Position = 2, Mandatory = $false)][decimal]$TimeDelay = 1
 )
-Clear-History; Clear-Host
+Clear-History; Clear-Host; $Error.Clear()
 $CurrentFolder = Get-Location
-Set-Location ($env:SystemRoot + "\System32")
+Set-Variable -Name SystemPath -Value ($env:SystemRoot + "\System32")
+Set-Location $SystemPath
 Set-Variable -Name LocalAdmin -Value $null
 Set-Variable -Name LocalAccounts -Value @()
 Set-Variable -Name LocalServices -Value @()
 Set-Variable -Name ListWebApps -Value @()
 Set-Variable -Name AppPoolUser -Value $null
-Set-Variable -Name UserDescription -Value "Local user account for Thycotic - Secret Server"
 $PSVersion = $PSVersionTable.PSVersion
 Write-Host ("Powershell version: " + $PSVersion)
 switch (($PSVersion).Major) {
@@ -23,53 +23,84 @@ switch (($PSVersion).Major) {
         $LocalAccounts += Get-WmiObject -Class Win32_UserAccount -Filter "LocalAccount = True"; Break
     }
 }
-[bool]$bFound = $false
 foreach ($Account in $LocalAccounts) {
     if ($Account.SID -like "*-500") {
         $LocalAdmin = $Account.Name
     }
-    if ($Account.Name -eq $ThycoticUser) {
-        $bFound = $true
+}
+Set-Variable -Name Robocopy -Value ($SystemPath + "\Robocopy.exe")
+Set-Variable -Name PShellPath -Value ($SystemPath + "\WindowsPowerShell\v1.0")
+Set-Variable -Name LocalModules -Value ($PShellPath + "\Modules")
+if (!(Test-Path ($LocalModules + "\ProcessCredentials"))) {
+    New-Item -Path ($LocalModules + "\ProcessCredentials") -ItemType Directory | Out-Null
+}
+Set-Variable -Name SourceFolder -Value ($env:USERPROFILE + "\Downloads")
+Set-Variable -Name FileName -Value "ProcessCredentials.psm1"
+$SourceURL = ("https://github.com/bstobie/techops/releases/download/Secure-String/" + $FileName)
+Set-Variable -Name Destination -Value ($SourceFolder + "\" + $FileName)
+if (!(Test-Path -Path $Destination)) {
+    try {
+        Invoke-WebRequest -Uri $SourceURL -OutFile $Destination
+    }
+    catch {
+        $Error.Clear()
     }
 }
-if (!($bFound)) {
-    $minLength = 16
-    $maxLength = 28
-    $nonAlphaChars = 5
-    $length = Get-Random -Minimum $minLength -Maximum $maxLength
-    try {
-        add-type -AssemblyName System.Web
-        $AccountPass = [System.Web.Security.Membership]::GeneratePassword($length, $nonAlphaChars)
-        $SecurePass = ConvertTo-SecureString -String $AccountPass -AsPlainText -Force
-        switch (($PSVersion).Major) {
-            {($_ -eq 5)} {
-                New-LocalUser -Name $ThycoticUser -Password $SecurePass -FullName "Secret Server" -Description $UserDescription | Out-Null
-                Add-LocalGroupMember -Group "Administrators" -Member $ThycoticUser
+Set-Variable -Name ModuleSrc -Value ($SourceFolder)
+Set-Variable -Name ModuleDes -Value ($LocalModules + "\ProcessCredentials")
+Set-Variable -Name Options -Value ($FileName + " /R:1 /W:5")
+$SrcFile = ($ModuleSrc + "\" + $FileName)
+$DesFile = ($ModuleDes + "\" + $FileName)
+if (!(Test-Path -Path $DesFile)) {
+    Start-Process -FilePath $Robocopy -ArgumentList ($ModuleSrc + " " + $ModuleDes + " " + $Options) -WindowStyle Hidden -Wait
+}
+else {
+    $Differences = Compare-Object -ReferenceObject (Get-Content -Path $SrcFile) -DifferenceObject (Get-Content -Path $DesFile)
+    foreach ($Difference in $Differences) {
+        if ($Difference.SideIndicator -eq " = >") {
+            try {
+                Start-Process -FilePath $Robocopy -ArgumentList ($ModuleSrc + " " + $ModuleDes + " " + $Options) -WindowStyle Hidden -Wait
                 Break
             }
-            Default {
-                $group = "Administrators"
-                $currentName = hostname
-                $objOu = [ADSI]"WinNT://$currentName"
-                $objUser = $objOU.Create("User", $ThycoticUser)
-                $objUser.setpassword($AccountPass)
-                Clear-Variable -Name "AccountPass"
-                $objUser.SetInfo()
-                $objUser.description = $UserDescription
-                $objUser.SetInfo()
-                $objOU = [ADSI]"WinNT://$currentName/$group"
-                $objOU.add("WinNT://$currentName/$($ThycoticUser)")
-                Break
+            catch {
+                $Error.Clear()
             }
         }
     }
-    catch {
-       $Error.Clear()
+}
+Import-Module ProcessCredentials
+$SvcAcctCreds = SetCredentials -SecureUser ("svc_lmdatacllctr@" + $LMDomain) -Domain $LMDomain
+if (!($SvcAcctCreds)) {$SvcAcctCreds = Get-Credential -Credential ("svc_lmdatacllctr@" + $LMDomain)}
+$Session = New-PSSession -ComputerName $CompAcct -Credential $SvcAcctCreds
+Set-Variable -Name SchTskTempFile -Value ("C:\Temp\SchTasks\SchTsk.csv")
+if (!(Test-Path -Path (Split-Path -Path $SchTskTempFile))) {
+    New-Item -Path (Split-Path -Path $SchTskTempFile) -ItemType Directory | Out-Null
+}
+$Script = {
+    param ($SchTskTempFile)
+    SchTasks.exe /query /V /FO CSV | Out-File $SchTskTempFile
+}
+try {
+    Invoke-Command -Session $Session -ScriptBlock $Script -ArgumentList $SchTskTempFile
+    $SchTaskList = Import-Csv -Path $SchTskTempFile
+    foreach ($SchTask in $SchTaskList) {
+        $SchTaskUser = ($SchTask.'Run As User')
+        if ($SchTaskUser -eq $LocalAdmin) {
+            $Message = ($SchTask.TaskName + " is using the 'Run As User': " + $SchTaskUser)
+            try {
+                Write-EventLog -LogName Application -Source "TechOps" -EventID 4673 -EntryType Error -Message $Message -Category 1 -RawData 10,20
+            }
+            catch {
+                $Error.Clear()
+            }
+        }
     }
-    finally {
-        Clear-Variable -Name "AccountPass"
-        $bFound = $true
-    }
+}
+catch {
+    $Error.Clear()
+}
+finally {
+    Remove-Item -Path $SchTskTempFile -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 }
 $WebApps = (Get-Module -ListAvailable webadmin*).Name
 if ($WebApps) {
